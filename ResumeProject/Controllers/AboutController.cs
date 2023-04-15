da0f732066b69cd2249d2eb3ac0a03939f1fd4f8
erhangndz <erhangndz@live.com>
2023-04-15 14:37:40 +0300

    updated

---
+++ ResumeProject/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResumeProject.Models;
+
+namespace ResumeProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            var x = db.TblProfile.ToList();
+            return View(x);
+        }
+
+        public PartialViewResult PartialTechs()
+        {
+            var x = db.TblTech.ToList();
+            return PartialView(x);
+        }
+    }
+
+}
