0ccc43f1744d0849776831e1eeb057fbceb47aef
erhangndz <erhangndz@live.com>
2023-04-15 12:02:23 +0300

    updated

--- ResumeProject/Controllers/ContactController.cs
+++ ResumeProject/Controllers/ContactController.cs
@@ -45,5 +45,37 @@ namespace ResumeProject.Controllers
             var x = db.TblContact.Find(id);
             return View(x);
         }
+
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            List<SelectListItem> values = (from x in db.TblCategory.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMessage(TblContact p)
+        {
+
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblContact.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Default");
+        }
+
+        public PartialViewResult PartialMap()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+        }
     }
 }
---
+++ ResumeProject/Controllers/ServiceController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResumeProject.Models;
+
+namespace ResumeProject.Controllers
+{
+    public class ServiceController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteService(int id)
+        {
+            var values = db.TblService.Find(id);
+            db.TblService.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var values = db.TblService.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateService(TblService p)
+        {
+            var x = db.TblService.Find(p.ServiceID);
+            x.ServiceTitle = p.ServiceTitle;
+            x.ServiceDescription = p.ServiceDescription;
+            x.ServiceIcon = p.ServiceIcon;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddService(TblService p)
+        {
+            db.TblService.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
